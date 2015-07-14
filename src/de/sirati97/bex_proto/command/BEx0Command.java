3d75d54ff8dbc740d21c57ee117869a3d35754af
TheSirati97 <TheSirati97@gmail.com>
2015-07-14 13:57:22 +0200

    Bugfix

--- src/de/sirati97/bex_proto/command/BEx0Command.java
+++ src/de/sirati97/bex_proto/command/BEx0Command.java
@@ -1,52 +1,32 @@
 package de.sirati97.bex_proto.command;
 
-import de.sirati97.bex_proto.ArrayType;
 import de.sirati97.bex_proto.ExtractorDat;
 import de.sirati97.bex_proto.MultiStream;
+import de.sirati97.bex_proto.NullStream;
 import de.sirati97.bex_proto.Stream;
 import de.sirati97.bex_proto.Type;
-import de.sirati97.bex_proto.TypeBase;
 import de.sirati97.bex_proto.network.NetConnection;
 
-public class BEx0Command<t1> implements CommandBase{
-	TypeBase[] types;
+public class BEx0Command implements CommandBase{
 	private short id;
 	private CommandBase parent;
 	
-	public BEx0Command(short id, TypeBase...types) {
-		this.types = types;
+	public BEx0Command(short id) {
 		this.id = id;
 	}
 	
-	@SuppressWarnings("unchecked")
 	public Void extract(ExtractorDat dat) {
-		Object[] r = new Object[1];
-		int counter=0;
-		for (TypeBase type:types) {
-			 Object tempObj = type.getExtractor().extract(dat);
-			 if (type instanceof ArrayType) {
-				 tempObj = ((ArrayType) type).toPremitiveArray(tempObj);
-			 }
-			 r[counter++] = tempObj;
-		}
-		receive((t1)r[0], dat.getSender());
+		receive(dat.getSender());
 		return null;
 	}
 	
-	public void receive(t1 arg1, NetConnection sender) {
+	public void receive(NetConnection sender) {
 		
 	}
 
 
-	public Stream send(t1 arg1) {
-		Stream[] streams = new Stream[types.length];
-		switch (types.length) {
-		case 1:
-			streams[0]=types[0].createStream(arg1);
-		default:
-			break;
-		}
-		return new MultiStream(streams);
+	public Stream send() {
+		return new NullStream();
 	}
 
 	@Override
@@ -76,8 +56,8 @@ public class BEx0Command<t1> implements CommandBase{
 	}
 	
 
-	public void send(t1 arg1, NetConnection... connections) {
-		send(send(arg1), connections);
+	public void send(NetConnection... connections) {
+		send(connections);
 	}
 	
 }
--- src/de/sirati97/bex_proto/command/BEx1Command.java
+++ src/de/sirati97/bex_proto/command/BEx1Command.java
@@ -1,32 +1,52 @@
 package de.sirati97.bex_proto.command;
 
+import de.sirati97.bex_proto.ArrayType;
 import de.sirati97.bex_proto.ExtractorDat;
 import de.sirati97.bex_proto.MultiStream;
-import de.sirati97.bex_proto.NullStream;
 import de.sirati97.bex_proto.Stream;
 import de.sirati97.bex_proto.Type;
+import de.sirati97.bex_proto.TypeBase;
 import de.sirati97.bex_proto.network.NetConnection;
 
-public class BEx1Command implements CommandBase{
+public class BEx1Command<t1> implements CommandBase{
+	TypeBase[] types;
 	private short id;
 	private CommandBase parent;
 	
-	public BEx1Command(short id) {
+	public BEx1Command(short id, TypeBase...types) {
+		this.types = types;
 		this.id = id;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public Void extract(ExtractorDat dat) {
-		receive(dat.getSender());
+		Object[] r = new Object[1];
+		int counter=0;
+		for (TypeBase type:types) {
+			 Object tempObj = type.getExtractor().extract(dat);
+			 if (type instanceof ArrayType) {
+				 tempObj = ((ArrayType) type).toPremitiveArray(tempObj);
+			 }
+			 r[counter++] = tempObj;
+		}
+		receive((t1)r[0], dat.getSender());
 		return null;
 	}
 	
-	public void receive(NetConnection sender) {
+	public void receive(t1 arg1, NetConnection sender) {
 		
 	}
 
 
-	public Stream send() {
-		return new NullStream();
+	public Stream send(t1 arg1) {
+		Stream[] streams = new Stream[types.length];
+		switch (types.length) {
+		case 1:
+			streams[0]=types[0].createStream(arg1);
+		default:
+			break;
+		}
+		return new MultiStream(streams);
 	}
 
 	@Override
@@ -56,8 +76,8 @@ public class BEx1Command implements CommandBase{
 	}
 	
 
-	public void send(NetConnection... connections) {
-		send(connections);
+	public void send(t1 arg1, NetConnection... connections) {
+		send(send(arg1), connections);
 	}
 	
 }
